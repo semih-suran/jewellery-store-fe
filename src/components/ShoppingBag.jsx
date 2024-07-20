f2036c20542d4192bad9c414487303274920693f
Semih Suran <semih.suran@gmail.com>
2024-07-20 08:38:11 +0100

    Changed some styling on rendered bag items

--- src/components/ShoppingBag.jsx
+++ src/components/ShoppingBag.jsx
@@ -1,10 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { ShoppingBagContext } from "./ShoppingBagContext";
-import { FaTrash, FaMinus, FaPlus } from "react-icons/fa";
+import { FaTrash, FaMinus, FaPlus, FaPaypal } from "react-icons/fa";
+import { motion, AnimatePresence } from "framer-motion";
 
 function ShoppingBag() {
-  const { bagItems, removeFromBag, clearBag, updateQuantity } = useContext(ShoppingBagContext);
+  const { bagItems, removeFromBag, clearBag, updateQuantity } =
+    useContext(ShoppingBagContext);
+  const [isRemoving, setIsRemoving] = useState({});
   const navigate = useNavigate();
 
   const calculateTotalPrice = () => {
@@ -18,6 +21,13 @@ function ShoppingBag() {
 
   const totalPrice = calculateTotalPrice();
 
+  const handleRemoveFromBag = (item) => {
+    setIsRemoving((prev) => ({ ...prev, [item.item_id]: true }));
+    setTimeout(() => {
+      removeFromBag(item.item_id);
+    }, 500);
+  };
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-100 p-8 pt-32">
       <div className="flex flex-col items-center justify-center h-full w-11/12 mx-auto">
@@ -26,64 +36,73 @@ function ShoppingBag() {
           <p>Your shopping bag is empty.</p>
         ) : (
           <div className="grid grid-cols-1 gap-4 mt-4 w-full">
-            {bagItems.map((item) => (
-              <div
-                key={item.item_id}
-                className="relative flex flex-col md:flex-row items-center justify-between p-4 bg-white shadow-md rounded-lg transform transition-transform duration-200 hover:scale-105 max-w-600px"
-                onClick={() => navigate(`/product/${item.item_id}`)}
-              >
-                <div className="flex flex-col w-full md:w-auto">
-                  <div className="flex items-center justify-between w-full">
-                    <div className="flex items-center">
-                      <h2 className="text-xl font-bold mr-2">{item.name}</h2>
-                      <p className="text-lg font-semibold text-gray-700">£{item.price}</p>
-                    </div>
-                  </div>
-                  <div className="flex items-center mt-2">
+            <AnimatePresence>
+              {bagItems.map((item) => (
+                <motion.div
+                  key={item.item_id}
+                  initial={{ opacity: 1 }}
+                  animate={{ opacity: isRemoving[item.item_id] ? 0 : 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ duration: 0.5 }}
+                  className="relative flex flex-col items-center justify-between p-4 bg-white shadow-md rounded-lg transform transition-transform duration-200 hover:scale-105 max-w-600px"
+                  onClick={() => navigate(`/product/${item.item_id}`)}
+                >
+                  <div className="flex items-center w-full">
                     <img
                       src={item.images_url[0]}
                       alt={item.name}
                       className="w-24 h-24 object-cover rounded-lg mr-4"
                     />
-                    <div className="flex items-center ml-auto">
+                    <div className="flex flex-col md:flex-row items-center justify-between w-full">
+                      <div className="flex items-center">
+                        <h2 className="font-bold mr-2">{item.name}</h2>
+                        <p className="font-semibold text-gray-700">
+                          £{item.price}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-between w-full mt-2">
+                    <div className="flex items-center">
                       <button
                         onClick={(e) => {
                           e.stopPropagation();
-                          removeFromBag(item.item_id);
+                          handleRemoveFromBag(item);
                         }}
-                        className="w-6 h-6 text-gray-500 mr-2"
+                        className="w-6 h-6 text-gray-500 mr-1"
                       >
-                        <FaTrash className="w-5 h-5 text-gray-500 hover:text-black" />
+                        <FaTrash className="w-4 h-4 text-gray-500 hover:text-black" />
                       </button>
                       <button
                         onClick={(e) => {
                           e.stopPropagation();
                           updateQuantity(item.item_id, item.quantity - 1);
                         }}
-                        className="text-xs bg-gray-200 text-gray-700 py-1 px-2 rounded-md hover:bg-gray-400 focus:outline-none"
+                        className="text-xs bg-gray-200 text-gray-700 py-1 px-1 rounded-md hover:bg-gray-400 focus:outline-none"
                       >
-                        <FaMinus />
+                        <FaMinus className="w-3 h-3" />
                       </button>
-                      <span className="mx-2">{item.quantity}</span>
+                      <span className="mx-1">{item.quantity}</span>
                       <button
                         onClick={(e) => {
                           e.stopPropagation();
                           updateQuantity(item.item_id, item.quantity + 1);
                         }}
-                        className="text-xs bg-gray-200 text-gray-700 py-1 px-2 rounded-md hover:bg-gray-400 focus:outline-none"
+                        className="text-xs bg-gray-200 text-gray-700 py-1 px-1 rounded-md hover:bg-gray-400 focus:outline-none"
                       >
-                        <FaPlus />
+                        <FaPlus className="w-3 h-3" />
                       </button>
                     </div>
-                  </div>
-                  <div className="flex justify-end items-center mt-2">
-                    <p className="ml-auto text-lg font-semibold text-gray-700">
-                      Total Price: £{(parseFloat(item.price.replace("£", "")) * item.quantity).toFixed(2)}
+                    <p className="font-semibold text-gray-700">
+                      Total Price: £
+                      {(
+                        parseFloat(item.price.replace("£", "")) * item.quantity
+                      ).toFixed(2)}
                     </p>
                   </div>
-                </div>
-              </div>
-            ))}
+                </motion.div>
+              ))}
+            </AnimatePresence>
             <div className="flex justify-center">
               <button
                 onClick={clearBag}
@@ -102,14 +121,14 @@ function ShoppingBag() {
         </Link>
         {bagItems.length > 0 && (
           <Link
-          to="#"
-          onClick={(e) => {
-            e.preventDefault();
-            alert("Under Development!");
-          }}
+            to="#"
+            onClick={(e) => {
+              e.preventDefault();
+              alert("Under Development!");
+            }}
             className="w-80 mt-4 py-2 px-4 rounded-md bg-green-500 text-white hover:bg-green-700 focus:outline-none flex items-center justify-center"
           >
-            {`Pay £${totalPrice}`}
+             <FaPaypal />{`ayPal £${totalPrice}`}
           </Link>
         )}
       </div>
