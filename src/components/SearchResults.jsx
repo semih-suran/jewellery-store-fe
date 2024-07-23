bf6b1e2d8c43062703a3c45533049ae50c7a4fa4
Semih Suran <semih.suran@gmail.com>
2024-07-23 21:13:16 +0100

    Search is fully working now

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,49 +1,44 @@
-import React, { useEffect, useState } from "react";
-import { InstantSearch, SearchBox, Hits, Configure } from "react-instantsearch";
+import React from "react";
 import algoliasearch from "algoliasearch/lite";
-import { Hit } from "./Hit";
+import "instantsearch.css/themes/satellite.css";
+import {
+  InstantSearch,
+  Hits,
+  Pagination,
+  Configure,
+} from "react-instantsearch";
 import { useLocation } from "react-router-dom";
+import { Hit } from "./Hit";
 
 const searchClient = algoliasearch(
   "L7WYB1CCJE",
   "33d8985a8c43c9fa5eecf3724380e9d4"
 );
 
-const useQuery = () => {
-  return new URLSearchParams(useLocation().search);
-};
-
 const SearchResults = () => {
-  const query = useQuery().get('query') || '';
-  const [searchState, setSearchState] = useState({ query });
-
-  useEffect(() => {
-    setSearchState({ query });
-  }, [query]);
+  const location = useLocation();
+  const query = new URLSearchParams(location.search).get("query");
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col pt-32">
+    <div className="max-w-4xl mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">Search Results</h1>
       <InstantSearch
         searchClient={searchClient}
+        future={{ preserveSharedStateOnUnmount: true }}
         indexName="jewellery-store"
-        searchState={searchState}
-        onSearchStateChange={setSearchState}
-        
       >
-        <Configure hitsPerPage={10} />
-        <main className="flex-grow flex flex-col items-center p-8">
-          <SearchBox
-            defaultRefinement={query}
-            classNames={{
-              root: "mb-4",
-              input: "w-full px-4 py-2 border border-gray-300 rounded-md",
-            }}
-          />
-          <Hits hitComponent={Hit} />
-        </main>
+        <Configure hitsPerPage={6} query={query} />
+        <Hits hitComponent={HitComponent} />
+        <Pagination />
       </InstantSearch>
     </div>
   );
 };
+
+const HitComponent = ({ hit }) => (
+  <div className="cursor-pointer border-b py-4">
+    <Hit hit={hit} />
+  </div>
+);
 
 export default SearchResults;
