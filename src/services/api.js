17a3e568ca2f0233e4669bcdee363675f5502e15
semih-suran <118595582+semih-suran@users.noreply.github.com>
2024-08-05 22:39:17 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import React from "react";
 import { motion } from "framer-motion";
 
-const BASE_URL = "http://localhost:9090/api";
-// const BASE_URL = process.env.REACT_APP_BASE_URL;
+// const BASE_URL = "http://localhost:9090/api";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const fetchAllItems = async () => {
   try {
